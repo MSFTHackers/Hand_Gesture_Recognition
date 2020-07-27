99d6d9e81aacc4b1737361f5d273bfb030c8e456
Sunil Patel <sunpate@microsoft.com>
2020-07-27 17:58:34 +0530

    added files

---
+++ extension3/mobilenet/index.js
@@ -0,0 +1,271 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+const fetch = require('node-fetch');
+
+import { IMAGENET_CLASSES } from './imagenet_classes.js';
+
+function Deque() {
+    this.stac = null;
+    this.len = 0;
+    this.size = function() {
+        return this.len;
+    }
+
+    this.pushback = function(item) {
+        // return;
+        if (this.stac === null) {
+            this.stac = item.expandDims(-1);
+        } else {
+            let temp2 = [this.stac, item.expandDims(-1)];
+            let temp = tf.concat3d(temp2, 2);
+            temp2.forEach(v => v.dispose());
+            this.stac.dispose();
+            this.stac = temp;
+        }
+        item.dispose();
+        this.len += 1;
+    }
+
+    this.popfront = function() {
+        // return;
+        this.len -= 1;
+        let stac2 = tf.slice3d(this.stac, [0, 0, 1], [64, 64, this.len]);
+        this.stac.dispose();
+        this.stac = stac2;
+    }
+}
+const buf = new Deque();
+
+
+$(() => {
+
+    function ViewModel() {
+        let self = this;
+        self.predictions = ko.observableArray([
+            { className: "s", probability: "12323" },
+            { className: "s", probability: "12323" },
+            { className: "s", probability: "12323" },
+        ]);
+        self.addItem = function(item) {
+            this.predictions.push(item);
+        }
+        self.clear = function() {
+            while (self.predictions().length != 0)
+                self.predictions.pop();
+        }
+    }
+
+    let vm = new ViewModel();
+    ko.applyBindings(vm);
+
+
+    const MOBILENET_MODEL_PATH =
+        // tslint:disable-next-line:max-line-length
+        'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json';
+    // 'https://chromeextensionsunil.blob.core.windows.net/extension/model.json';
+
+    const IMAGE_SIZE = 224;
+    const TOPK_PREDICTIONS = 10;
+
+    let mobilenet;
+
+    function predictResult() {
+        const catElement = document.getElementById('cat');
+        if (catElement.complete && catElement.naturalHeight !== 0) {
+            predict(catElement);
+            catElement.style.display = '';
+        } else {
+            catElement.onload = () => {
+                predict(catElement);
+                catElement.style.display = '';
+            }
+        }
+    }
+    const mobilenetDemo = async() => {
+        status('Loading model...');
+
+        mobilenet = await tf.loadLayersModel(MOBILENET_MODEL_PATH);
+        // mobilenet = await tf.loadLayersModel("https://chromeextensionsunil.blob.core.windows.net/extension/model.json");
+        // Warmup the model. This isn't necessary, but makes the first prediction
+        // faster. Call `dispose` to release the WebGL memory allocated for the return
+        // value of `predict`.
+        mobilenet.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3])).dispose();
+
+        status('');
+
+        // Make a prediction through the locally hosted cat.jpg.
+        predictResult();
+
+        document.addEventListener("event", async e => {
+            let grayImage = await transformGrayScale(document.getElementById("canvas"));
+            if (buf.len == 30)
+                buf.popfront();
+            buf.pushback(grayImage);
+            grayImage.dispose();
+            predictResult();
+        });
+
+        document.getElementById("snap").addEventListener("click", e => {
+
+
+        });
+
+        // document.getElementById('file-container').style.display = '';
+    };
+
+    /**
+     * Given an image element, makes a prediction through mobilenet returning the
+     * probabilities of the top K classes.
+     */
+    function transform(imgElement) {
+        let img = tf.browser.fromPixels(imgElement).toFloat();
+
+        const offset = tf.scalar(127.5);
+        // Normalize the image from [0, 255] to [-1, 1].
+        let normalized = img.sub(offset).div(offset);
+
+        // Reshape to a single-element batch so we can pass it to predict.
+        let batched = normalized.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
+        return batched;
+    }
+
+    async function transformGrayScale(imgElement) {
+        // img will be in the form [1 w h 1]
+        let img2 = tf.tidy(() => {
+            let img = tf.browser.fromPixels(imgElement)
+                .mean(2)
+                .toFloat().div(tf.scalar(255.0));
+
+            const offset = tf.scalar(127.5);
+            img = img.expandDims(-1);
+
+            img = tf.image.resizeBilinear(img, [64, 64]);
+
+            img = img.squeeze(-1);
+            return img;
+        });
+        await tf.browser.toPixels(img2, document.getElementById("canvas2"));
+        return img2;
+    }
+
+    async function predict(imgElement) {
+        console.log(buf.len + " " + tf.memory().numBytes / 1024 + "KB");
+        status('Predicting...');
+
+        // The first start time includes the time it takes to extract the image
+        // from the HTML and preprocess it, in additon to the predict() call.
+        const startTime1 = performance.now();
+        // The second start time excludes the extraction and preprocessing and
+        // includes only the predict() call.
+        let startTime2;
+        const logits = tf.tidy(() => {
+            // tf.browser.fromPixels() returns a Tensor from an image element.
+            startTime2 = performance.now();
+            // Make a prediction through mobilenet.
+            return mobilenet.predict(transform(imgElement));
+        });
+
+        // Convert logits to probabilities and class names.
+        const classes = await getTopKClasses(logits, TOPK_PREDICTIONS);
+        const totalTime1 = performance.now() - startTime1;
+        const totalTime2 = performance.now() - startTime2;
+        status(`Done in ${Math.floor(totalTime1)} ms ` +
+            `(not including preprocessing: ${Math.floor(totalTime2)} ms)`);
+
+        // Show the classes in the DOM.
+        showResults(imgElement, classes);
+    }
+
+    /**
+     * Computes the probabilities of the topK classes given logits by computing
+     * softmax to get probabilities and then sorting the probabilities.
+     * @param logits Tensor representing the logits from MobileNet.
+     * @param topK The number of top predictions to show.
+     */
+    async function getTopKClasses(logits, topK) {
+        const values = await logits.data();
+        logits.dispose();
+        const valuesAndIndices = [];
+        for (let i = 0; i < values.length; i++) {
+            valuesAndIndices.push({ value: values[i], index: i });
+        }
+        valuesAndIndices.sort((a, b) => {
+            return b.value - a.value;
+        });
+        const topkValues = new Float32Array(topK);
+        const topkIndices = new Int32Array(topK);
+        for (let i = 0; i < topK; i++) {
+            topkValues[i] = valuesAndIndices[i].value;
+            topkIndices[i] = valuesAndIndices[i].index;
+        }
+
+        const topClassesAndProbs = [];
+        for (let i = 0; i < topkIndices.length; i++) {
+            topClassesAndProbs.push({
+                className: IMAGENET_CLASSES[topkIndices[i]],
+                probability: topkValues[i]
+            })
+        }
+        return topClassesAndProbs;
+    }
+
+    //
+    // UI
+    //
+
+    function showResults(imgElement, classes) {
+        vm.clear();
+
+        for (let i = 0; i < classes.length; i++)
+            vm.addItem({ className: classes[i].className, probability: classes[i].probability.toFixed(3) });
+        // console.log(vm.predictions);
+    }
+
+
+
+    // const filesElement = document.getElementById('files');
+    // filesElement.addEventListener('change', evt => {
+    //     let files = evt.target.files;
+    //     // Display thumbnails & issue call to predict each image.
+    //     for (let i = 0, f; f = files[i]; i++) {
+    //         // Only process image files (skip non image files)
+    //         if (!f.type.match('image.*')) {
+    //             continue;
+    //         }
+    //         let reader = new FileReader();
+    //         reader.onload = e => {
+    //             // Fill the image & call predict.
+    //             let img = document.createElement('img');
+    //             img.src = e.target.result;
+    //             img.width = IMAGE_SIZE;
+    //             img.height = IMAGE_SIZE;
+    //             img.onload = () => predict(img);
+    //         };
+
+    //         // Read in the image file as a data URL.
+    //         reader.readAsDataURL(f);
+    //     }
+    // });
+
+    const demoStatusElement = document.getElementById('status');
+    const status = msg => demoStatusElement.innerText = msg;
+
+    const predictionsElement = document.getElementById('predictions');
+
+    mobilenetDemo();
+
+});
